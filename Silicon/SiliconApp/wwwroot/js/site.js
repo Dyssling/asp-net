6f16adc108c0e1465c0f218877437522a3caad99
Dyssling <olle.lundgarden@hotmail.com>
2024-02-21 15:25:31 +0100

    Header implemented into MVC app.

--- Silicon/SiliconApp/wwwroot/js/site.js
+++ Silicon/SiliconApp/wwwroot/js/site.js
@@ -1,4 +1,9 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function toggleMenu() {
+    if (document.getElementById("menu").style.display != "block") {
+        document.getElementById("menu").style.display = "block";
+    }
 
-// Write your JavaScript code.
+    else {
+        document.getElementById("menu").style.display = "none";
+    }
+}
