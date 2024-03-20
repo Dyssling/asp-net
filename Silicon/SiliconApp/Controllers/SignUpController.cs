bf29824e38826da48c8f20bf7017a15c3b043851
Dyssling <olle.lundgarden@hotmail.com>
2024-03-20 13:53:46 +0100

    All pages implemented into MVC project. TODO: Fix links

---
+++ Silicon/SiliconApp/Controllers/SignUpController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SiliconApp.Controllers
+{
+    public class SignUpController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Silicon/SiliconApp/Controllers/SingleCourseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SiliconApp.Controllers
+{
+    public class SingleCourseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
