44785648be68c21f7adf6b2b3ac51b8dbd4a57b7
Dyssling <olle.lundgarden@hotmail.com>
2024-04-08 20:20:23 +0200

    Signed out users can no longer access courses page.

--- Silicon/SiliconApp/Controllers/CoursesController.cs
+++ Silicon/SiliconApp/Controllers/CoursesController.cs
@@ -1,11 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SiliconApp.Services;
 
 namespace SiliconApp.Controllers
 {
     public class CoursesController : Controller
     {
+        private readonly UserService _userService;
+
+        public CoursesController(UserService userService)
+        {
+            _userService = userService;
+        }
+
         public IActionResult Index()
         {
+            if (!_userService.IsUserSignedIn(User))
+            {
+                return RedirectToRoute(new { controller = "Account", action = "SignIn" }); //Om användaren är utloggad redirectas man till Sign In sidan
+            }
+
             ViewData["Title"] = "Courses";
 
             return View();
