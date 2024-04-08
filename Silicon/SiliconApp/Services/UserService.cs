80eb8eeedf92e66e0f43ce61b43d2c41456d35e8
Dyssling <olle.lundgarden@hotmail.com>
2024-04-08 16:17:16 +0200

    UserService created. TODO: Implement it in the account controller.

---
+++ Silicon/SiliconApp/Services/UserService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using SiliconApp.Entities;
+using SiliconApp.ViewModels;
+
+namespace SiliconApp.Services
+{
+    public class UserService
+    {
+        private readonly UserManager<UserEntity> _userManager;
+
+        public UserService(UserManager<UserEntity> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<string> CreateNewUserAsync(SignUpViewModel viewModel)
+        {
+            var exists = await _userManager.Users.AnyAsync(x => x.Email == viewModel.Form.Email);
+
+            if (exists)
+            {
+                return "A user with the same email already exists.";
+            }
+
+            var userEntity = new UserEntity()
+            {
+                FirstName = viewModel.Form.FirstName,
+                LastName = viewModel.Form.LastName,
+                Email = viewModel.Form.Email,
+                UserName = viewModel.Form.Email
+            };
+
+            var result = await _userManager.CreateAsync(userEntity, viewModel.Form.Password);
+
+            if (result.Succeeded)
+            {
+                return "Success!";
+            }
+
+            return "An error occurred while creating the user.";
+
+        }
+    }
+}
