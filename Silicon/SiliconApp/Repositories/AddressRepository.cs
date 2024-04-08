7b57e5df5a4bc54a29cf5fa599a80cac3e81fb74
Dyssling <olle.lundgarden@hotmail.com>
2024-04-08 12:09:28 +0200

    User and Address repository created.

---
+++ Silicon/SiliconApp/Repositories/AddressRepository.cs
@@ -0,0 +1,11 @@
+﻿using SiliconApp.Contexts;
+
+namespace SiliconApp.Repositories
+{
+    public class AddressRepository : BaseRepository<AddressRepository, DataContext>
+    {
+        public AddressRepository(DataContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ Silicon/SiliconApp/Repositories/UserRepository.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+using Microsoft.EntityFrameworkCore;
+using SiliconApp.Contexts;
+using SiliconApp.Entities;
+using System.Diagnostics;
+using System.Linq.Expressions;
+
+namespace SiliconApp.Repositories
+{
+    public class UserRepository : BaseRepository<UserEntity, DataContext>
+    {
+        private readonly DataContext _context;
+        public UserRepository(DataContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        //I nedanstående overrides  gör jag en Include för att göra en JOIN sats på User entiteterna man får tillbaka, så att man även får tillbaka dess addressinformation.
+
+        public override async Task<IEnumerable<UserEntity>> GetAllAsync()
+        {
+            try
+            {
+                var entityList = await _context.Users.Include(x => x.Address).ToListAsync();
+                return entityList;
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return null!;
+            }
+        }
+
+        public override async Task<UserEntity> GetOneAsync(Expression<Func<UserEntity, bool>> expression)
+        {
+            try
+            {
+                var entity = await _context.Users.Include(x => x.Address).FirstOrDefaultAsync(expression);
+                if (entity != null)
+                {
+                    return entity;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+            return null!;
+        }
+    }
+}
