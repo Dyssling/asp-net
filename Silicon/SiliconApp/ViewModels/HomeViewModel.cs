3ab0fe519ec52e001b45cdd18dbb3e35ba427d53
Dyssling <olle.lundgarden@hotmail.com>
2024-04-08 09:49:00 +0200

    Newsletter validation finished. Also added fragments to form posts so that page scrolls back down to form after post. Wrong branch don't care

---
+++ Silicon/SiliconApp/ViewModels/HomeViewModel.cs
@@ -0,0 +1,9 @@
+﻿using SiliconApp.Models;
+
+namespace SiliconApp.ViewModels
+{
+    public class HomeViewModel
+    {
+        public HomeNewsletterModel Form { get; set; } = new HomeNewsletterModel();
+    }
+}
