f7b41c678f2025f54d6da60d69ed445268e535b3
Dyssling <olle.lundgarden@hotmail.com>
2024-04-12 22:17:12 +0200

    Light and dark mode finished with switch and cookie. Wrong branch dont give a darn

---
+++ Silicon/SiliconApp/Controllers/SiteSettings.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SiliconApp.Controllers
+{
+    public class SiteSettings : Controller
+    {
+        public IActionResult ChangeTheme(string theme) //Här tas theme parametern emot, som skickades från vår fetch
+        {
+            var options = new CookieOptions()
+            {
+                Expires = DateTime.Now.AddDays(60)
+            };
+
+            try
+            {
+                Response.Cookies.Append("ThemeMode", theme, options); //Lägg till cookien MED theme parametern
+
+                return Ok();
+            }
+
+            catch //Om något skulle råka gå snett så får man bara ett 404, vilket inte kommer göra någonting mer i detta fallet än att inte gå vidare i vår JS funktion
+            {
+                return NotFound();
+            }
+        }
+    }
+}
--- Silicon/SiliconApp/wwwroot/js/site.js
+++ Silicon/SiliconApp/wwwroot/js/site.js
@@ -1,4 +1,30 @@
-﻿function toggleMenu() {
+﻿let sw = document.getElementById("theme-switch");               //Hämtar både desktop och mobil switcharna
+let swMobile = document.getElementById("theme-switch-mobile");
+
+sw.addEventListener("change", () => { //Lägg till en event listener på desktop switchen, som kör funktionen när värdet ändras
+    let theme = sw.checked ? "dark" : "light"; //Om den är bockad så sätts theme variabeln till dark, annars light
+
+    fetch(`/sitesettings/changetheme?theme=${theme}`) //En fetch utförs, som alltså kommer hämta en respons från följande URL, vilket i detta fall innebär att ChangeTheme action kommer köras, i SiteSettings controllern
+        .then(response => { //En fetch är asynkron, men NÄR den är färdig så vill vi göra en check på responsen
+            if (response.ok) {
+                window.location.reload(); //Om allting lyckades, och vi får tillbaka en Ok respons från ChangeTheme, så laddas sidan om
+            }
+        });
+});
+
+swMobile.addEventListener("change", () => {
+    let theme = swMobile.checked ? "dark" : "light";
+
+    fetch(`/sitesettings/changetheme?theme=${theme}`)
+        .then(response => {
+            if (response.ok) {
+                window.location.reload();
+            }
+        });
+});
+
+
+const toggleMenu = () => {
     if (document.getElementById("menu").style.display != "block") {
         document.getElementById("menu").style.display = "block";
     }
@@ -6,4 +32,4 @@
     else {
         document.getElementById("menu").style.display = "none";
     }
-}
+};
